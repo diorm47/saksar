0ca8228e48e4314eda48c30552e268f0eb6d0668
dior <dior47m@gmail.com>
2022-12-11 21:27:44 +0500

    added sending form to email

--- src/components/application-from/application_form.jsx
+++ src/components/application-from/application_form.jsx
@@ -1,57 +1,79 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import Snackbar from "../snackbar/snackbar";
 import "./application_form.css";
 
 function ApplicationForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const [snackbar, setSnackbar] = useState(false);
+  const [snackbarText, setSnackbarText] = useState("");
+  const SERVICE_ID = "service_07yzqro";
+  const TEMPLATE_ID = "template_iy74582";
+  const USER_ID = "DSFmx66MwidL4OS8S";
+
+  let form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        setSnackbarText("Отправлен!!!");
+        setSnackbar(true);
+        setTimeout(() => {
+          setSnackbar(false);
+        }, 2000);
+        form.current.reset();
+      },
+      (error) => {
+        console.log(error.text);
+        setSnackbarText("Ошибка! Повторите заново!");
+        setSnackbar(true);
+        setTimeout(() => {
+          setSnackbar(false);
+        }, 2000);
+        form.current.reset();
+      }
+    );
   };
+
   return (
-    <div className="footer_form">
-      <div className="footer_title">
-        <h3>
-          Если нужного товара нет в наличии, отправьте заявку на <br />
-          предзаказ в этой форме
-        </h3>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="name_input">
-          <input
-            placeholder="Ваше имя"
-            {...register("name", { required: true })}
-          />
-          {errors.name && <span>Поля имя обьязятельный</span>}
-        </div>
-        <div className="phone_input">
-          <input
-            placeholder="Ваш телефон"
-            {...register("phone_number", { required: true })}
-          />
-          {errors.phone_number && <span>Поля телефона обьязятельный</span>}
-        </div>
-        <div className="product_name_input">
-          <input
-            placeholder="Название товара"
-            {...register("product_name", { required: true })}
-          />
-          {errors.product_name && <span>Поля товара обьязятельный</span>}
-        </div>
-        <div className="email_input">
-          <input
-            placeholder="Ваш e-mail"
-            {...register("email", { required: true })}
-          />
-          {errors.product_name && <span>Поля e-mail обьязятельный</span>}
+    <>
+      <div className="footer_form">
+        <div className="footer_title">
+          <h3>
+            Если нужного товара нет в наличии, отправьте заявку на <br />
+            предзаказ в этой форме
+          </h3>
         </div>
 
-        <input className="submit_btn" type="submit" value="Отправить заявку" />
-      </form>
-    </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="name_input">
+            <input placeholder="Ваше имя" required name="name" type="text" />
+          </div>
+          <div className="phone_input">
+            <input name="phone_number" required placeholder="Ваш телефон" />
+          </div>
+          <div className="product_name_input">
+            <input placeholder="Название товара" required name="product_name" />
+          </div>
+          <div className="email_input">
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="Ваш e-mail"
+            />
+          </div>
+
+          <input
+            className="submit_btn"
+            type="submit"
+            value="Отправить заявку"
+          />
+        </form>
+      </div>
+      {!snackbar || <Snackbar props={snackbarText} />}
+    </>
   );
 }
 
--- src/pages/contacts-page/contacts-page.jsx
+++ src/pages/contacts-page/contacts-page.jsx
@@ -1,22 +1,47 @@
-import React from "react";
-import "./contacts-page.css";
-import { useForm } from "react-hook-form";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import Snackbar from "../../components/snackbar/snackbar";
 import instagramm_icon from "../../assets/icons/instagramm_icon.png";
 import vk_icon from "../../assets/icons/vk_icon.png";
 import Footer from "../../components/footer/footer";
+import "./contacts-page.css";
 
 function ContactsPage() {
   React.useEffect(() => {
     document.title = `Наши контакты`;
   }, []);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const [snackbar, setSnackbar] = useState(false);
+  const [snackbarText, setSnackbarText] = useState("");
+  const SERVICE_ID = "service_07yzqro";
+  const TEMPLATE_ID = "template_iy74582";
+  const USER_ID = "DSFmx66MwidL4OS8S";
+
+  let form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        setSnackbarText("Отправлен!!!");
+        setSnackbar(true);
+        setTimeout(() => {
+          setSnackbar(false);
+        }, 2000);
+        form.current.reset();
+      },
+      (error) => {
+        console.log(error.text);
+        setSnackbarText("Ошибка! Повторите заново!");
+        setSnackbar(true);
+        setTimeout(() => {
+          setSnackbar(false);
+        }, 2000);
+        form.current.reset();
+      }
+    );
   };
+
   return (
     <div className="contacts_page">
       <div className="contacts_title">
@@ -26,27 +51,15 @@ function ContactsPage() {
         <h1>Оставьте заявку</h1>
         <p>Заполните поля формы, и мы свяжемся с вами</p>
         <div className="contacts_form">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="contacts_email_input">
-              <input
-                placeholder="e-mail"
-                {...register("email", { required: true })}
-              />
-              {errors.email && <span>Поля e-mail обьязятельный</span>}
+              <input placeholder="e-mail" type="email" name="email" required />
             </div>
             <div className="contacts_name_input">
-              <input
-                placeholder="имя"
-                {...register("name", { required: true })}
-              />
-              {errors.name && <span>Поля имя обьязятельный</span>}
+              <input placeholder="имя" required name="name" type="text" />
             </div>
             <div className="contacts_phone_input">
-              <input
-                placeholder="телефон"
-                {...register("phone_number", { required: true })}
-              />
-              {errors.phone_number && <span>Поля телефона обьязятельный</span>}
+              <input placeholder="телефон" name="phone_number" required />
             </div>
 
             <div className="contacts_submit_btn">
@@ -98,6 +111,7 @@ function ContactsPage() {
         </p>
       </div>
       <Footer />
+      {!snackbar || <Snackbar props={snackbarText} />}
     </div>
   );
 }
