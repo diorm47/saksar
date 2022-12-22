f36ff403d10d41bbe0a934b370fd4267a0d0e9fc
dior <dior47m@gmail.com>
2022-12-22 15:39:36 +0500

    cards fix

--- src/components/home_products_links/home-products.jsx
+++ src/components/home_products_links/home-products.jsx
@@ -8,41 +8,41 @@ import { NavLink } from "react-router-dom";
 function HomeProducts() {
   return (
     <>
-      <div className="home_product_cart">
-        <div className="home_product_cart_img">
-          <img src={aromadiffuzors} alt="Аромадиффузоры" />
-        </div>
-        <div className="home_product_cart_title">
-          <p>Аромадиффузоры</p>
-          <NavLink to="/diffusers">
-            <span>Перейти в каталог</span>
-          </NavLink>
-        </div>
-      </div>
+      <NavLink to="/diffusers">
+        <div className="home_product_cart">
+          <div className="home_product_cart_img">
+            <img src={aromadiffuzors} alt="Аромадиффузоры" />
+          </div>
+          <div className="home_product_cart_title">
+            <p>Аромадиффузоры</p>
 
-      <div className="home_product_cart">
-        <div className="home_product_cart_img">
-          <img src={scrabs} alt="scrabs" />
-        </div>
-        <div className="home_product_cart_title">
-          <p>Скрабы</p>
-          <NavLink to="/scrubs">
             <span>Перейти в каталог</span>
-          </NavLink>
+          </div>
         </div>
-      </div>
+      </NavLink>
+      <NavLink to="/scrubs">
+        <div className="home_product_cart">
+          <div className="home_product_cart_img">
+            <img src={scrabs} alt="scrabs" />
+          </div>
+          <div className="home_product_cart_title">
+            <p>Скрабы</p>
+            <span>Перейти в каталог</span>
+          </div>
+        </div>
+      </NavLink>
+      <NavLink to="/spa-sets">
+        <div className="home_product_cart">
+          <div className="home_product_cart_img">
+            <img src={spa} alt="spa" />
+          </div>
+          <div className="home_product_cart_title">
+            <p>Spa-наборы для тела</p>
 
-      <div className="home_product_cart">
-        <div className="home_product_cart_img">
-          <img src={spa} alt="spa" />
-        </div>
-        <div className="home_product_cart_title">
-          <p>Spa-наборы для тела</p>
-          <NavLink to="/spa-sets">
             <span>Перейти в каталог</span>
-          </NavLink>
+          </div>
         </div>
-      </div>
+      </NavLink>
     </>
   );
 }
